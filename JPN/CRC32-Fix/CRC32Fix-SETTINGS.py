8c93e1ebf34fac69525aae2525ead25ddea3e816
ChampionLeake <ChampionLeake@users.noreply.github.com>
2019-02-22 16:07:48 -0500

    Update CRC32Fix-SETTINGS.py

--- JPN/CRC32-Fix/CRC32Fix-SETTINGS.py
+++ JPN/CRC32-Fix/CRC32Fix-SETTINGS.py
@@ -10,7 +10,7 @@ import os
 import zlib
 
 for i in glob.iglob('../build/UNOSetting.sav', recursive=True):
-    if os.path.getsize(i) == 0x2618:	#size of the overall UNOSetting.sav file
+    if os.path.getsize(i) == 0x2618:	#size of the overall UNOMatch.sav file
         with open(i, 'rb+') as f:
             data = f.read(0x2614)	#reads the offset of the CRC-bytes
             if data[0x160:0x168] == b'\xff\xff\xff\xff\xff\xff\xff\xff':	#sanity-check for a specific string to be found in the save (specifically offset 0x160) ; string is 8 bytes long
